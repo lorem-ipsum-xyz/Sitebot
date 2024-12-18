2fcdab6969688254d2e13313b948ffaef1fd7d0d
Greegmon <greegmon13@gmail.com>
2024-12-19 06:45:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,11 @@ socket, app = myapp()
 
 if __name__ == '__main__':
   PORT = os.getenv('PORT', 5000)
-  socket.run(app, host='0.0.0.0', port=PORT, debug=True)
+  #socket.run(app, host='0.0.0.0', port=PORT, debug=True)
+  socket.run(
+    app,
+    host='0.0.0.0',
+    port=PORT,
+    debug=True,
+    allow_unsafe_werkzeug=True
+  )
