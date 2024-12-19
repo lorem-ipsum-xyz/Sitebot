be278e9d6bf280a18589f1cd1e89b32001945ce0
Greegmon <greegmon13@gmail.com>
2024-12-19 09:05:45 +0800

    Fix shoti, attemp 2

--- commands/shoti.py
+++ commands/shoti.py
@@ -6,7 +6,7 @@ import json
 def getShoti(link):
   try:
     url = "https://ttsave.app/download"
-    headers = {"Content-Type": 'application/json',"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"}
+    headers = {"Content-Type": 'application/json',"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"}
     data = {"query": link,"languange_id": "1"}
     
     res = post(url, json=data, headers=headers, timeout=10)
@@ -15,16 +15,19 @@ def getShoti(link):
     buttons = html.find('div', id='button-download-ready')
     mdiv = html.find('div', class_="flex flex-row items-center justify-center gap-1 mt-5 w-3/4")
     row = html.find('div',class_="flex flex-row items-center justify-center gap-2 mt-2")
+    
+    if html.text == 'Error unknown':
+      return {"error": "⚠️ Error while proccessing the data"}
     cols = row.find_all('div',class_='flex-row')
     
     username = html.find('a', class_="font-extrabold text-blue-400 text-xl mb-2").text
     description = html.find('p', class_="text-gray-600 px-2 text-center break-all w-3/4 oneliner").text or 'No description'
-    views = cols[0].text.strip() or 0
-    comments = cols[2].text.strip() or 0
-    shares = cols[4].text.strip() or 0
-    music = mdiv.find('span', class_="text-gray-500").text
+    views = cols[0].text.strip() or '0'
+    comments = cols[2].text.strip() or '0'
+    shares = cols[4].text.strip() or '0'
+    music = mdiv.find('span', class_="text-gray-500").text or 'unknown'
     videoLink = html.find('a', attrs={'type':"no-watermark"}).get('href')
-    return {
+    data = {
       "username": username,
       "description": description,
       "views": views,
@@ -33,6 +36,8 @@ def getShoti(link):
       "music": music,
       "videoSource": videoLink
     }
+    print(data)
+    return data
   except Exception as e:
     print("ERROR: ", e)
     return {"error": str(e)}
@@ -49,13 +54,13 @@ def shoti(bot, data):
       return bot.sendMessage(res['error'])#("⚠️ An error accured while fetching the data, please try again.")
     line = "━━━━━━━━━━━━━━━"
     text = line + '\n'
-    text += "username: " + res['username'] + '\n'
-    text += "views: " + res['views'] + '\n'
-    text += "comments: " + res['comments'] + '\n'
-    text += "shares: " + res['shares'] + '\n'
-    text += "music: " + res['music'] + '\n'
+    text += "username: " + res.get('username') + '\n'
+    text += "views: " + res.get('views') + '\n'
+    text += "comments: " + res.get('comments') + '\n'
+    text += "shares: " + res.get('shares') + '\n'
+    text += "music: " + res.get('music') + '\n'
     text += line + '\n'
-    text += res['description'] if res['description'] != 'No description' else None
+    text += res.get('description') if res.get('description') != 'No description' else ''
     message = {
       "body": text,
       "attachment": {
